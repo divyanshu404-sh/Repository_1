a379e5847279d88474ef7c7fa498492f17ef4848
DIVYANSHU SHEKHAR <223066235+divyanshu404-sh@users.noreply.github.com>
2025-08-27 00:14:14 +0530

    Add files via upload

---
+++ Day3.1.c
@@ -0,0 +1,15 @@
+// Write a program to convert temperature from Celsius to Fahrenheit.
+
+#include <stdio.h>
+
+int main(){
+
+    float celsius;
+
+    printf("Enter temperature in celsius: \n");
+    scanf("%f",&celsius);
+
+    printf("Temperature in fahrenheit is %f",((celsius*9)/5+32));
+
+    return 0;
+}
---
+++ Day3.2.c
@@ -0,0 +1,21 @@
+// Write a program to swap two numbers using a third variable.
+
+#include <stdio.h>
+
+int main(){
+
+    int a,b,c;
+
+    printf("Enter two numbers: \n");
+    scanf("%d %d",&a,&b);
+
+    printf("Before swapping a = %d and b = %d\n",a,b);
+
+    c=a;
+    a=b;
+    b=c;
+     
+    printf("After swapping a = %d amd b = %d",a,b);
+
+    return 0;
+}
